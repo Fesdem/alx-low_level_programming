d1f6bc4d0edb357656e9d9131076adb74d85536b
Fesdem <festusdemiladeoni@gmail.com>
2023-06-30 00:11:48 +0000

    Task to perform pointers and arrays

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int);
+
+#endif  /* MAIN_H_ */
